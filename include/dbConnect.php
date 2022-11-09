a289df7f945056b9a6470775341a74c2a76c6b5c
nawelwelwel <99800086+nawelwelwel@users.noreply.github.com>
2022-11-09 09:58:52 +0100

    Update dbConnect.php

--- include/dbConnect.php
+++ include/dbConnect.php
@@ -4,7 +4,7 @@ session start();
 $con=mysqli_connect("database-1.cmeydtszbdvp.us-east-1.rds.amazonaws.com","hotel","rsidm2g1","hotel");
 
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Hotel-Management-System-Ajax-PHP-Mysql-master/');
-define('SITE_PATH','http://54.89.75.7/Hotel-Management-System-Ajax-PHP-Mysql-master/');
+define('SITE_PATH','http://3.84.54.222/Hotel-Management-System-Ajax-PHP-Mysql-master/');
 
 
 //$server="localhost";
